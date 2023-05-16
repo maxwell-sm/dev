c7dde0942c43a83bccf6a7af4f6ec5d0a07a7c00
maxwell-sm <59855181+maxwell-sm@users.noreply.github.com>
2023-05-15 21:24:36 -0300

    Add files via upload

---
+++ primeiroProjeto.rb
@@ -0,0 +1,10 @@
+#Captura de dados
+print "Digite seu Nome: "
+nome = gets.chomp
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua Idade: "
+idade = gets.chomp.to_i
+
+#Exibição de dados
+puts "Nome: #{nome} #{sobrenome} tem #{idade} anos de idade"
